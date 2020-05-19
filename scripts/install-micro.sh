bd3f254ac25830fcc061ba042b558ac572745c2e
Mustafa Saber <mostafa.saber3789@gmail.com>
2020-05-19 15:03:02 +0200

    fix k8s version for agones

--- scripts/install-micro.sh
+++ scripts/install-micro.sh
@@ -1,4 +1,4 @@
-sudo snap install microk8s --classic
+sudo snap install microk8s --classic --channel=1.14/stable
 # sudo snap alias microk8s.kubectl kubectl
 sudo usermod -a -G microk8s $USER
 sudo microk8s.status --wait-ready
@@ -7,6 +7,8 @@ sudo microk8s.kubectl get services
 sudo microk8s.kubectl get all --all-namespaces
 sudo microk8s.kubectl get no
 sudo microk8s.enable dns dashboard registry
+
+# Connect outer kubectl to inner kubectl
 sudo microk8s.kubectl config view --raw > $HOME/.kube/config
 
 echo "logout to be able to access this without sudo"
